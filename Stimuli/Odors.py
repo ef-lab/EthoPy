3f17ce262344753f129cd8acf07d0109f233f28d
eman <olakiril@hotmail.com>
2020-03-13 18:21:12 +0200

    logging of odors

--- Stimuli/Odors.py
+++ Stimuli/Odors.py
@@ -6,7 +6,16 @@ class Odors(Stimulus):
 
     def prepare(self):
         self.probes = np.array([d['probe'] for d in self.conditions])
-        self.logger.log_conditions('OdorCond', self.conditions)
+        conditions = self.conditions
+        for icond, cond in enumerate(conditions):
+            values = list(cond.values())
+            names = list(cond.keys())
+            for ivalue, value in enumerate(values):
+                if type(value) is list:
+                    value = tuple(value)
+                cond.update({names[ivalue]: value})
+            conditions[icond] = cond
+        self.logger.log_conditions('OdorCond', conditions)
 
     def init(self):
         delivery_idx = self.curr_cond['delivery_idx']
