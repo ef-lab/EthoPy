022c9d6539db7c512fbf91ca24ca9414f8912558
eman <olakiril@hotmail.com>
2020-04-30 17:03:55 +0300

    fix threading

--- Logger.py
+++ Logger.py
@@ -12,12 +12,6 @@ dj.config["enable_python_native_blobs"] = True
 class Logger:
     """ This class handles the database logging"""
 
-    def __init__(self):
-        self.thread_runner = threading.Thread(target=self.inserter)  # max insertion rate of 10 events/sec
-        self.thread_runner.start()
-        self.thread_end = threading.Event()
-        self.thread_lock = Lock()
-
     def init_params(self):
         pass
 
@@ -93,7 +87,10 @@ class RPLogger(Logger):
         conn2 = dj.Connection(connect_info['database.host'], connect_info['database.user'],
                               connect_info['database.password'])
         self.insert_schema = dj.create_virtual_module('beh.py', 'lab_behavior', connection=conn2)
-        super(RPLogger, self).__init__()
+        self.thread_runner = threading.Thread(target=self.inserter)  # max insertion rate of 10 events/sec
+        self.thread_runner.start()
+        self.thread_end = threading.Event()
+        self.thread_lock = Lock()
 
     def init_params(self):
         pass
