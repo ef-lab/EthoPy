9f60a6fcebd7830669901cdb1f559f581752359d
eman <olakiril@hotmail.com>
2020-05-06 13:55:03 +0300

    Interface

--- Experiments/Calibrate.py
+++ Experiments/Calibrate.py
@@ -16,9 +16,9 @@ class Calibrate:
         print('Running calibration')
 
         for cal_idx in range(0, numpy.size(self.params['pulsenum'])):
-            self.screen.draw('', 400, 0, 400, 480, (0, 128, 0), 40)
+            self.screen.draw('Place zero-weighted pad', 400, 0, 400, 480, (0, 128, 0), 40)
             button = self.screen.add_button(name='OK', x=500, y=300, w=100, h=100)
-
+            self.screen.run()
             while not button.is_pressed():
                 time.sleep(0.2)
 
