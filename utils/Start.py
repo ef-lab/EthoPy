99baa560a4d986d6417ec1fb95fdc3b1a950cd34
eman <olakiril@hotmail.com>
2020-05-11 11:14:21 +0300

    correct Start.py

--- utils/Start.py
+++ utils/Start.py
@@ -1,5 +1,6 @@
 from utils.Interface import *
 import time, os, socket
+from utils.Timer import Timer
 
 class Welcome:
     def __init__(self, logger):
@@ -7,18 +8,17 @@ class Welcome:
         self.screen = Interface()
         self.start()
         self.state = ''
+        self.timer = Timer()
+        self.animal = 0
+        self.task = 0
 
     def setup(self):
-        animal = self.logger.get_setup_animal()
-        task = self.logger.get_setup_task()
-        hostname = socket.gethostname()
-        ip = socket.gethostbyname(hostname)
         self.cleanup()
-        self.screen.draw('%s %s' % (self.logger.ip, self.logger.setup), 0, 0, 150, 100, (128, 128, 128), size=15)
-        self.screen.add_button(name='Animal %d' % animal, action=self.change_animal,
-                               x=250, y=80, w=200, h=100, color=(0, 0, 0), font_size=30)
-        self.screen.add_button(name='Task %d' % task, action=self.change_task,
+        self.screen.add_button(name='Animal %d' % self.animal, action=self.change_animal,
+                                   x=250, y=80, w=200, h=100, color=(0, 0, 0), font_size=30)
+        self.screen.add_button(name='Task %d' % self.task, action=self.change_task,
                                x=250, y=160, w=200, h=100, color=(0, 0, 0), font_size=30)
+        self.screen.draw('%s %s' % (self.logger.ip, self.logger.setup), 0, 0, 150, 100, (128, 128, 128), size=15)
         self.screen.add_button(name='Start experiment', action=self.start_experiment,
                                x=250, y=330, w=200, h=100, color=(0, 128, 0))
         self.screen.add_button(name='Restart', action=self.reboot, x=700, y=340, w=100, h=70,
@@ -64,9 +64,18 @@ class Welcome:
                 self.logger.update_state('stopped')
                 self.screen.exit()
             else:
-                self.screen.draw('%s %s' % (self.logger.ip, self.logger.setup),
-                                 0, 0, 150, 100, (128, 128, 128), size=15)
+                self.update_setup_info()
                 time.sleep(0.2)
+        else:
+            self.screen.ts.stop()
+
+    def update_setup_info(self):
+        animal = self.logger.get_setup_animal()
+        task = self.logger.get_setup_task()
+        if self.animal != animal or self.task != task:
+            self.animal = animal
+            self.task = task
+            self.setup()
 
     def start_experiment(self):
         self.state = 'start_experiment'
