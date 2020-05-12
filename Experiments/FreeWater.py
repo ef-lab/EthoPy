e8f03442c0ae605d6e49655e3263ca4410350e4d
eman <olakiril@hotmail.com>
2020-05-13 00:35:24 +0300

    testing free water

---
+++ Experiments/FreeWater.py
@@ -0,0 +1,163 @@
+from utils.Timer import *
+from StateMachine import *
+from datetime import datetime, timedelta
+
+
+class State(StateClass):
+    def __init__(self, parent=None):
+        self.timer = Timer()
+        if parent:
+            self.__dict__.update(parent.__dict__)
+
+    def setup(self, logger, BehaviorClass, StimulusClass, session_params, conditions):
+
+        logger.log_session(session_params, conditions)
+
+        # Initialize params & Behavior/Stimulus objects
+        self.logger = logger
+        self.beh = BehaviorClass(logger, session_params)
+        self.stim = StimulusClass(logger, session_params, conditions, self.beh)
+        self.params = session_params
+        self.StateMachine = StateMachine(Prepare(self), Exit(self))
+
+        # Initialize states
+        global states
+        states = {
+            'PreTrial'     : PreTrial(self),
+            'Trial'        : Trial(self),
+            'InterTrial'   : InterTrial(self),
+            'Reward'       : Reward(self),
+            'Sleep'        : Sleep(self),
+            'Exit'         : Exit(self)}
+
+    def entry(self):  # updates stateMachine from Database entry - override for timing critical transitions
+        self.StateMachine.status = self.logger.get_setup_status()
+        self.logger.update_state(self.__class__.__name__)
+
+    def run(self):
+        self.StateMachine.run()
+
+    def is_sleep_time(self):
+        now = datetime.now()
+        t = datetime.strptime(self.params['start_time'], "%H:%M:%S")
+        start = now.replace(hour=0, minute=0, second=0) + timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+        t = datetime.strptime(self.params['stop_time'], "%H:%M:%S")
+        stop = now.replace(hour=0, minute=0, second=0) + timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+        if stop < start:
+            stop = stop + timedelta(days=1)
+        return now < start or now > stop
+
+
+class Prepare(State):
+    def run(self):
+        self.stim.setup()
+
+    def next(self):
+        if self.is_sleep_time():
+            return states['Sleep']
+        else:
+            return states['PreTrial']
+
+
+class PreTrial(State):
+    def entry(self):
+        self.timer.start()
+        self.logger.update_state(self.__class__.__name__)
+
+    def run(self): pass
+
+    def next(self):
+        if self.beh.is_ready(self.params['init_duration']):
+            return states['Trial']
+        else:
+            self.StateMachine.status = self.logger.get_setup_status()
+            return states['PreTrial']
+
+
+class Trial(State):
+    def __init__(self, parent):
+        self.__dict__.update(parent.__dict__)
+        self.stim.unshow()
+        self.is_ready = 0
+        self.probe = 0
+        self.resp_ready = False
+        super().__init__()
+
+    def entry(self):
+        self.logger.update_state(self.__class__.__name__)
+        self.beh.is_licking()
+        self.timer.start()  # trial start counter
+        self.logger.start_trial(self.stim.curr_cond['cond_idx'])
+        self.logger.thread_lock.acquire()
+
+    def run(self):
+        self.stim.present()  # Start Stimulus
+        self.is_ready = self.beh.is_ready(self.timer.elapsed_time())  # update times
+        self.probe = self.beh.is_licking()
+        if self.timer.elapsed_time() > self.params['delay_duration'] and not self.resp_ready:
+            self.resp_ready = True
+            if self.probe > 0: self.beh.update_bias(self.probe)
+
+    def next(self):
+        if self.probe > 0 and self.resp_ready: # response to correct probe
+            return states['Reward']
+        elif self.timer.elapsed_time() > self.params['trial_duration']:      # timed out
+            return states['InterTrial']
+        else:
+            return states['Trial']
+
+    def exit(self):
+        self.logger.thread_lock.release()
+        self.logger.log_trial()
+        self.stim.unshow((0, 0, 0))
+
+
+class InterTrial(State):
+    def run(self):
+        if self.beh.is_licking():
+            self.timer.start()
+
+    def next(self):
+        if self.is_sleep_time():
+            return states['Sleep']
+        elif self.timer.elapsed_time() > self.params['intertrial_duration']:
+            return states['PreTrial']
+        else:
+            return states['InterTrial']
+
+
+class Reward(State):
+    def run(self):
+        self.beh.reward()
+        self.stim.stop()
+
+    def next(self):
+        return states['InterTrial']
+
+
+class Sleep(State):
+    def entry(self):
+        self.logger.update_state(self.__class__.__name__)
+        self.logger.update_setup_status('offtime')
+        self.stim.unshow([0, 0, 0])
+
+    def run(self):
+        self.logger.ping()
+        time.sleep(5)
+
+    def next(self):
+
+        if self.is_sleep_time() and self.logger.get_setup_status() == 'offtime':
+            return states['Sleep']
+        elif self.logger.get_setup_status() == 'offtime':
+            self.logger.update_setup_status('running')
+            self.stim.unshow()
+            return states['Exit']
+        else:
+            return states['PreTrial']
+
+
+class Exit(State):
+    def run(self):
+        self.beh.cleanup()
+        self.stim.unshow()
---
+++ conf/free_water.py
@@ -0,0 +1,25 @@
+from Experiments.FreeWater import *
+from Behavior import *
+from Stimuli.RPMovies import *
+
+# define session parameters
+session_params = {
+    'trial_duration'     : 10000,
+    'intertrial_duration': 0,
+    'timeout_duration'   : 4000,
+    'stim_duration'      : 3000,
+    'delay_duration'     : 500,
+    'response_interval'  : 1000,
+    'init_duration'      : 0,
+    'reward_amount'      : 10,
+    'randomization'      : 'bias',
+    'start_time'         : '10:00:00',
+    'stop_time'          : '18:00:00',
+    'reward'             : 'water',
+}
+
+# run experiment
+exp = State()
+exp.setup(logger, RPBehavior, RPMovies, session_params)
+exp.run()
+
