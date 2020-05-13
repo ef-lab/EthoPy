f8da6f3ef58861fd346f4af67e779f5c06f09855
eman <olakiril@hotmail.com>
2020-05-13 14:18:31 +0300

    Free water implementation

--- conf/free_water.py
+++ conf/free_water.py
@@ -1,16 +1,15 @@
 from Experiments.FreeWater import *
 from Behavior import *
-from Stimuli.RPMovies import *
 
 # define session parameters
 session_params = {
     'trial_duration'     : 10000,
     'intertrial_duration': 0,
-    'timeout_duration'   : 4000,
-    'stim_duration'      : 3000,
-    'delay_duration'     : 500,
+    'timeout_duration'   : 0,
+    'stim_duration'      : 0,
+    'delay_duration'     : 0,
     'response_interval'  : 1000,
-    'init_duration'      : 0,
+    'init_duration'      : 100,
     'reward_amount'      : 10,
     'randomization'      : 'bias',
     'start_time'         : '10:00:00',
