7016d7c562a34d51929154720a83d6994b6ee104
eman <olakiril@hotmail.com>
2020-05-29 13:40:47 +0300

    sleep check in pretrial

--- conf/calibrate_probes.py
+++ conf/calibrate_probes.py
@@ -5,7 +5,7 @@ from Experiments.Calibrate import *
 session_params = {
     'duration'        : [20, 40, 100, 250],
     'probes'          : [1, 2],
-    'pulsenum'        : [300, 200, 80, 20],
+    'pulsenum'        : [600, 400, 100, 30],
     'pulse_interval'  : [50, 50, 100, 100],
     'save'            : True,
 }
