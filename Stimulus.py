48226b2411392ff3b7e42d8238ab16f432e86c55
eman <olakiril@hotmail.com>
2020-07-09 14:48:20 +0300

    reward history bug

--- Stimulus.py
+++ Stimulus.py
@@ -78,7 +78,7 @@ class Stimulus:
             if len(h) < self.params['bias_window']: h = np.mean(self.probes)
             mn = np.min(self.probes); mx = np.max(self.probes)
             bias_probe = np.random.binomial(1, 1 - np.nanmean((h - mn) / (mx - mn))) * (mx - mn) + mn
-            if self.iter == 0 or np.size(self.iter) == 0:
+            if self.iter == 1 or np.size(self.iter) == 0:
                 self.iter = self.params['staircase_window']
                 perf = np.nanmean(np.greater(rew_h[-self.params['staircase_window']:], 0))
                 if perf > self.params['stair_up'] and self.curr_difficulty < max(self.difficulties):
@@ -86,8 +86,9 @@ class Stimulus:
                 elif perf < self.params['stair_down'] and self.curr_difficulty > min(self.difficulties):
                     self.curr_difficulty -= 1
                 self.logger.update_difficulty(self.curr_difficulty)
-            if self.beh.probe_history[-1] > 0:
-                self.iter -= 1
+            else:
+                if self.beh.probe_history[-1:] > 0:
+                    self.iter -= 1
             selected_conditions = [i for (i, v) in zip(self.conditions, np.logical_and(self.probes == bias_probe,
                                                        np.array(self.difficulties) == self.curr_difficulty)) if v]
             self.curr_cond = np.random.choice(selected_conditions)
