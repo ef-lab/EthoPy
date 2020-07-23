3c7b26c476f4966c16d9a965413c306e3c145962
eman <olakiril@hotmail.com>
2020-07-23 18:10:14 +0300

    Free water support within 2AFC

--- Stimulus.py
+++ Stimulus.py
@@ -92,9 +92,8 @@ class Stimulus:
             selected_conditions = [i for (i, v) in zip(self.conditions, np.logical_and(self.probes == bias_probe,
                                                        np.array(self.difficulties) == self.curr_difficulty)) if v]
             self.curr_cond = np.random.choice(selected_conditions)
-        elif self.params['trial_selection'] == 'staircase_nobias':
-            idx = ~np.isnan(self.beh.probe_history)
-            rew_h = self.beh.reward_history[idx]
+        elif self.params['trial_selection'] == 'water_stairs':
+            rew_h = self.beh.reward_history
             if self.iter == 1 or np.size(self.iter) == 0:
                 self.iter = self.params['staircase_window']
                 perf = np.nanmean(np.greater(rew_h[-self.params['staircase_window']:], 0))
@@ -104,8 +103,7 @@ class Stimulus:
                     self.curr_difficulty -= 1
                 self.logger.update_difficulty(self.curr_difficulty)
             else:
-                if self.beh.probe_history[-1:] > 0:
-                    self.iter -= 1
+                self.iter -= 1
             selected_conditions = [i for (i, v) in zip(self.conditions,
                                                        np.array(self.difficulties) == self.curr_difficulty) if v]
             self.curr_cond = np.random.choice(selected_conditions)
