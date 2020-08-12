40c00706667d7e4be04e3c126df1c2ddc4c32ba5
eman <olakiril@hotmail.com>
2020-08-12 10:14:22 +0300

    ef-rp1 center port reverse, Abort trial

--- Probe.py
+++ Probe.py
@@ -158,7 +158,7 @@ class RPProbe(Probe):
         self.Pulser.wave_clear()
 
     def getStart(self):
-        if self.logger.setup == 'ef-rp9':
+        if numpy.any(self.logger.setup == numpy.array(['ef-rp9','ef-rp1'])):
             return not self.GPIO.input(self.channels['start'][1])
         else:
             return self.GPIO.input(self.channels['start'][1])
