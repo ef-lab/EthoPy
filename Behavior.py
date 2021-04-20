d52a0f91ce63df74d356b5afabf3fd4cec57c1c1
eman <olakiril@hotmail.com>
2021-04-20 11:58:45 +0300

    VR changes

--- Behavior.py
+++ Behavior.py
@@ -285,7 +285,7 @@ class VRBehavior(Behavior):
 
     def is_ready(self):
         x, y = self.get_position()
-        in_position = ((self.resp_loc_x - x)**2 + (self.resp_loc_y - y)**2)**.5 < self.radius
+        in_position = any(((self.resp_loc_x - x)**2 + (self.resp_loc_y - y)**2)**.5 < self.radius)
         return in_position
 
     def is_correct(self, condition):
