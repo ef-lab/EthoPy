ebe8d0e151f37ecf2834e28ceb0889d80c6304ce
sofiamor <med2p1080156@med.uoc.gr>
2021-07-14 16:39:54 +0300

    VR changes :)

--- core/Stimulus.py
+++ core/Stimulus.py
@@ -47,6 +47,7 @@ class Stimulus:
 
     def start(self):
         """start stimulus"""
+        self.beh = BehaviorClass()
         self.isrunning = True
         self.logger.log('StimCondition.Trial', dict(period='', stim_hash=self.curr_cond['stim_hash']),
                         schema='stimulus')
