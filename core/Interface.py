39e15c9d97f4bed834412cb910221db34f06c7c4
eman <olakiril@hotmail.com>
2021-11-05 15:09:18 +0200

    fix odor_channels

--- core/Interface.py
+++ core/Interface.py
@@ -221,22 +221,22 @@ class VRProbe(RPProbe):
 
     def start_odor(self, channels, dutycycle=50, frequency=20):
         self.frequency = frequency
-        self.channels = channels
+        self.odor_channels = channels
         for channel in channels:
             self.pwm[channel] = self.GPIO.PWM(self.channels['odor'][channel], self.frequency)
             self.pwm[channel].ChangeFrequency(self.frequency)
             self.pwm[channel].start(dutycycle)
 
     def update_odor(self, dutycycles):  # for 2D olfactory setup
-        for channel, dutycycle in zip(self.channels, dutycycles):
+        for channel, dutycycle in zip(self.odor_channels, dutycycles):
             self.pwm[channel].ChangeDutyCycle(dutycycle)
 
     def stop_odor(self):
-        for channel in self.channels:
+        for channel in self.odor_channels:
             self.pwm[channel].stop()
 
     def cleanup(self):
-        for channel in self.channels:
+        for channel in self.odor_channels:
             self.pwm[channel].stop()
         super().cleanup()
 
