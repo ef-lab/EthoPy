f91d51a963585e03ddb7b6da59792a65682cd473
sofiamor <med2p1080156@med.uoc.gr>
2022-03-17 17:34:10 +0200

    new changes

---
+++ Behaviors/VRBalltest.py
@@ -0,0 +1,119 @@
+from core.Behavior import *
+from core.Interface import *
+
+
+@behavior.schema
+class VRBall(Behavior, dj.Manual):
+    definition = """
+    # This class handles the behavior variables for RP
+    ->BehCondition
+    ---
+    x_sz                 : float
+    y_sz                 : float
+    x0                   : float
+    y0                   : float  
+    x_t                  : float
+    y_t                  : float          
+    theta0               : float
+    radius               : float
+    speed_thr            : float # in m/sec
+    """
+
+    class Response(dj.Part):
+        definition = """
+        # Lick response condition
+        -> VRBall
+        response_loc_y            : float            # response y location
+        response_loc_x            : float            # response x location
+        response_port             : tinyint          # response port id
+        """
+
+    class Reward(dj.Part):
+        definition = """
+        # reward port conditions
+        -> VRBall
+        ---
+        reward_loc_x              : float
+        reward_loc_y              : float
+        reward_port               : tinyint          # reward port id
+        reward_amount=0           : float            # reward amount
+        reward_type               : varchar(16)      # reward type
+        """
+
+    cond_tables = ['VRBall', 'VRBall.Response', 'VRBall.Reward']
+    required_fields = ['x0', 'y0', 'x_t', 'y_t','radius', 'response_loc_y', 'response_loc_x',
+                       'reward_loc_x', 'reward_loc_y', 'reward_amount']
+    default_key = {'reward_type': 'water', 'speed_thr': 0.025,
+                   'response_port': 1, 'reward_port': 1, 'theta0': 0}
+
+    def setup(self, exp):
+        self.previous_x = 0
+        self.previous_y = 0
+        self.resp_loc_x = None
+        self.resp_loc_y = None
+        self.interface = VRProbe(exp=exp)
+        super(VRBall, self).setup(exp)
+        self.vr = Ball(exp)
+        self.flag = True
+
+    def prepare(self, condition):
+        if condition['x_t'] < 0 or condition['y_t'] < 0:
+            x_t, y_t, theta0, time = self.vr.getPosition()
+            self.vr.setPosition(condition['x_sz'], condition['y_sz'], x_t, y_t, theta0)
+        else:
+            self.vr.setPosition(condition['x_sz'], condition['y_sz'], condition['x0'], condition['y0'],
+                                condition['theta0'])
+        super().prepare(condition)
+
+    def is_ready(self):
+        x, y, theta, tmst = self.get_position()
+        in_position = False
+        for r_x, r_y in zip(self.curr_cond['response_loc_x'], self.curr_cond['response_loc_y']):
+            in_position = in_position or np.sum((np.array([r_x, r_y]) - [x, y]) ** 2) ** .5 < self.curr_cond['radius']
+        return in_position
+
+    def is_running(self):
+        return self.vr.getSpeed() > self.curr_cond['speed_thr']
+
+    def is_correct(self):
+        x, y, theta, tmst = self.get_position()
+        in_position = False
+        if self.flag == True:
+            print('first time')
+            for c_x, c_y in zip(self.curr_cond['response_loc_x'], self.curr_cond['response_loc_y']):
+                in_position = np.sum((np.array([c_x, c_y]) - [x, y]) ** 2) ** .5 < self.curr_cond['radius']
+                if in_position:
+                    self.prev_loc_x = x
+                    self.prev_loc_y = y
+                    print(self.prev_loc_x, self.prev_loc_y)
+                    break
+            self.flag=False
+        elif not self.flag:
+            print('another time')
+            for c_x, c_y in zip(self.curr_cond['response_loc_x'], self.curr_cond['response_loc_y']):
+                prev_position = np.sum((np.array([c_x, c_y]) - [self.prev_loc_x, self.prev_loc_y]) ** 2) ** .5 < self.curr_cond['radius']
+                temp_in_position = np.sum((np.array([c_x, c_y]) - [x, y]) ** 2) ** .5 < self.curr_cond['radius']
+                print(x, self.prev_loc_x,y, self.prev_loc_y)
+                if temp_in_position and not prev_position:
+                    in_position = True
+                    self.prev_loc_x = x
+                    self.prev_loc_y = y
+                    break
+        return in_position
+
+    def get_position(self):
+        return self.vr.getPosition()
+
+    def reward(self):
+        self.interface.give_liquid(self.licked_port)
+        self.log_reward(self.reward_amount[self.licked_port])
+        self.update_history(self.licked_port, self.reward_amount[self.licked_port])
+
+    def punish(self):
+        self.update_history(self.licked_port)
+
+    def exit(self):
+        self.vr.cleanup()
+        self.interface.cleanup()
+
+
---
+++ Experiments/Explore.py
@@ -0,0 +1,164 @@
+from core.Experiment import *
+
+
+@experiment.schema
+class Condition(dj.Manual):
+    class Navigate(dj.Part):
+        definition = """
+        # Navigation experiment conditions
+        -> Condition
+        ---
+        trial_selection='staircase' : enum('fixed','random','staircase','biased') 
+        bias_window=5               : smallint
+        staircase_window=20         : smallint
+        stair_up=0.7                : float
+        stair_down=0.55             : float
+        noresponse_intertrial=1     : tinyint(1)
+        norun_response=1            : tinyint(1)
+        incremental_punishment=1    : tinyint(1)
+    
+        difficulty                  : int   
+        trial_ready                 : int
+        trial_duration              : int
+        intertrial_duration         : int
+        reward_duration             : int
+        punish_duration             : int
+        """
+
+
+class Experiment(State, ExperimentClass):
+    cond_tables = ['Navigate']
+    required_fields = []
+    default_key = {'trial_selection'       : 'staircase',
+                   'bias_window'           : 5,
+                   'staircase_window'      : 10,
+                   'stair_up'              : 0.7,
+                   'stair_down'            : 0.55,
+                   'noresponse_intertrial' : True,
+                   'norun_response'        : True,
+                   'incremental_punishment': True,
+                   'difficulty'             : 0,
+                   'trial_ready'            : 0,
+                   'intertrial_duration'    : 1000,
+                   'trial_duration'         : 1000,
+                   'reward_duration'        : 2000,
+                   'punish_duration'        : 1000}
+
+    def entry(self):  # updates stateMachine from Database entry - override for timing critical transitions
+        self.logger.curr_state = self.name()
+        self.start_time = self.logger.log('Trial.StateOnset', {'state': self.name()})
+        self.resp_ready = False
+        self.state_timer.start()
+
+
+class Entry(Experiment):
+    def entry(self):
+        pass
+
+    def next(self):
+        return 'PreTrial'
+
+
+class PreTrial(Experiment):
+    def entry(self):
+        self.prepare_trial()
+        if not self.is_stopped():
+            self.beh.prepare(self.curr_cond)
+            self.stim.prepare(self.curr_cond)
+            self.logger.ping()
+            self.state_timer.start()
+
+    def next(self):
+        if self.is_stopped():
+            return 'Exit'
+        else:
+            return 'Trial'
+
+
+class Trial(Experiment):
+    def entry(self):
+        super().entry()
+        self.stim.start()
+
+    def run(self):
+        self.stim.present()
+        self.response = self.beh.is_licking(self.start_time)
+        time.sleep(.1)
+
+    def next(self):
+        if self.response and self.beh.is_correct() and not self.beh.is_running():  # correct response
+            return 'Reward'
+        elif self.state_timer.elapsed_time() > self.stim.curr_cond['trial_duration']:  # timed out
+            return 'Abort'
+        elif self.is_stopped():
+            return 'Exit'
+        else:
+            return 'Trial'
+
+    def exit(self):
+        self.stim.stop()
+        self.logger.ping()
+
+
+class Abort(Experiment):
+    def entry(self):
+        pass
+
+    def run(self):
+        self.beh.update_history()
+        self.logger.log('Trial.Aborted')
+
+    def next(self):
+        return 'InterTrial'
+
+
+class Reward(Experiment):
+    def run(self):
+        self.beh.reward()
+
+    def next(self):
+        return 'InterTrial'
+
+
+class Punish(Experiment):
+    def entry(self):
+        self.beh.punish()
+        super().entry()
+
+    def run(self):
+        self.stim.punish_stim()
+
+    def next(self):
+        if self.state_timer.elapsed_time() >= self.stim.curr_cond['punish_duration']:
+            return 'InterTrial'
+        else:
+            return 'Punish'
+
+    def exit(self):
+        self.stim.unshow()
+
+
+class InterTrial(Experiment):
+    def entry(self):
+        self.state_timer.start()
+
+    def run(self):
+        if self.beh.get_response(self.start_time) & self.params.get('noresponse_intertrial'):
+            self.state_timer.start()
+
+    def next(self):
+        if self.is_stopped():
+            return 'Exit'
+        elif self.beh.is_running():
+            return 'InterTrial'
+        elif self.state_timer.elapsed_time() >= self.stim.curr_cond['intertrial_duration']:
+            return 'PreTrial'
+        else:
+            return 'InterTrial'
+
+
+class Exit(Experiment):
+    def run(self):
+        self.beh.exit()
+        self.stim.exit()
+        self.logger.ping(0)
