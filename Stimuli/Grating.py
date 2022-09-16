c59499a411cf2553b75b258adcdfac694e262e20
Your Name <you@example.com>
2022-09-16 15:02:00 +0300

    Deleted doublicate statements

--- Stimuli/Grating.py
+++ Stimuli/Grating.py
@@ -202,7 +202,7 @@ class GratingRP(Grating):
         if not pygame.get_init():
             pygame.init()
         self.screen = pygame.display.set_mode(self.size)
-        self.unshow() ##Here!
+        self.unshow() 
         pygame.mouse.set_visible(0)
         pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
@@ -265,18 +265,6 @@ class GratingRP(Grating):
     def get_clip_info(self, key, table, *fields):
         key['file_name'] = self._get_filename(key)
         return self.exp.logger.get(schema='stimulus', table=table, key=key, fields=fields)
-    
-    def ready_stim(self):
-        self.unshow([i*256 for i in self.monitor['ready_color']])
-
-    def reward_stim(self):
-        self.unshow([i*256 for i in self.monitor['reward_color']])
-
-    def punish_stim(self):
-        self.unshow([i*256 for i in self.monitor['punish_color']])
-
-    def start_stim(self):
-        self.unshow([i*256 for i in self.monitor['start_color']])
 
 
 class GratingOld(Grating):
