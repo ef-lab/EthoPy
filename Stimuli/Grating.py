44684f7cc15f3180d5381fb27464eb317fd6008e
Emmanouil Froudarakis <olakiril@hotmail.com>
2023-09-18 10:29:49 +0300

    testing moving grating

--- Stimuli/Grating.py
+++ Stimuli/Grating.py
@@ -47,6 +47,44 @@ class Grating(Stimulus, dj.Manual):
                               'reward': (128, 128, 128),
                               'punish': (0, 0, 0)})
 
+    def make_conditions(self, conditions=[]):
+        self.path = os.path.dirname(os.path.abspath(__file__)) + '/movies/'
+        if not os.path.isdir(self.path):  # create path if necessary
+            os.makedirs(self.path)
+        super().make_conditions(conditions)
+        for cond in conditions:
+            if cond['temporal_freq'] != 0:
+                filename = self._get_filename(cond)
+                tuple = self.exp.logger.get(schema='stimulus', table='Grating.Movie',
+                                            key={**cond, 'file_name': filename}, fields=['stim_hash'])
+                if not tuple:
+                    print('Making movie %s', filename)
+                    cond['lamda'] = int(self.px_per_deg/cond['spatial_freq'])
+                    theta_frame_step = (cond['temporal_freq'] / self.fps) * np.pi * 2
+                    image = self._make_grating(**cond)
+                    images = image[:self.monitor['resolution_x'], :self.monitor['resolution_y']]
+                    if cond['flatness_correction']:
+                        images, transform = flat2curve(images, self.monitor['monitor_distance'],
+                                                    self.monitor['monitor_size'], method='index',
+                                                    center_x=self.monitor['monitor_center_x'],
+                                                       center_y=self.monitor['monitor_center_y'])
+                        images = self._gray2rgb(images)
+                    else:
+                        transform = lambda x: x
+                    for iframe in range(0, int(cond['duration']*self.fps/1000 + 10)):
+                        print('\r' + ('frame %d/%d' % (iframe, int(cond['duration']*self.fps/1000 + 10))), end='')
+                        cond['phase'] += theta_frame_step
+                        image = self._make_grating(**cond)
+                        images = np.dstack((images, self._gray2rgb(transform(image[:self.monitor['resolution_x'],
+                                                                                   :self.monitor['resolution_y']]))))
+                    print('\r' + 'done!')
+                    images = np.transpose(images[:, :, :], [2, 1, 0])
+                    self._im2mov(self.path + filename, images)
+                    self.logger.log('Grating.Movie', {**cond, 'file_name': filename,
+                                                      'clip': np.fromfile(self.path + filename, dtype=np.int8)},
+                                    schema='stimulus', priority=2, block=True, validate=True)
+        return conditions
+
     def init(self, exp):
         super().init(exp)
         self.size = (self.monitor['resolution_x'], self.monitor['resolution_y'])    # window size
@@ -60,33 +98,43 @@ class Grating(Stimulus, dj.Manual):
 
     def prepare(self, curr_cond):
         self.isrunning = True
+        self.movie = False
         self.frame_idx = 0
         self.clock = pygame.time.Clock()
         curr_cond['lamda'] = int(self.px_per_deg / curr_cond['spatial_freq'])
-        image = self._make_grating(**curr_cond)
-        image = image[:self.monitor['resolution_x'], :self.monitor['resolution_y']]
-        if curr_cond['flatness_correction']:
-            image, transform = flat2curve(image, self.monitor['monitor_distance'],
-                                      self.monitor['monitor_size'], method='index',
-                                      center_x=self.monitor['monitor_center_x'],
-                                      center_y=self.monitor['monitor_center_y'])
-            image = image[:self.monitor['resolution_x'], :self.monitor['resolution_y']]
-        self.grating = self.Presenter.make_surface(self._gray2rgb(image, 3))
-        assert curr_cond['temporal_freq'] == 0
-            #print('Not optimized!')
-            #self.frame_step = curr_cond['lamda'] * (self.curr_cond['temporal_freq'] / self.fps)
-            #self.xt = np.cos((self.curr_cond['theta'] / 180) * np.pi)
-            #self.yt = np.sin((self.curr_cond['theta'] / 180) * np.pi)
         self.curr_cond = curr_cond
+        if curr_cond['temporal_freq'] != 0:
+            image = self._make_grating(**curr_cond)
+            image = image[:self.monitor['resolution_x'], :self.monitor['resolution_y']]
+            if curr_cond['flatness_correction']:
+                image, transform = flat2curve(image, self.monitor['monitor_distance'],
+                                          self.monitor['monitor_size'], method='index',
+                                          center_x=self.monitor['monitor_center_x'],
+                                          center_y=self.monitor['monitor_center_y'])
+                image = image[:self.monitor['resolution_x'], :self.monitor['resolution_y']]
+            self.grating = self.Presenter.make_surface(self._gray2rgb(image, 3))
+        else:
+            self.movie = True
+            self.curr_cond.update(dict(filename=self._get_filename(curr_cond)))
+            clip = self.exp.logger.get(schema='stimulus', table='Grating.Movie', key=self.curr_cond, fields=['clip'])
+            self.vid = imageio.get_reader(io.BytesIO(clip[0].tobytes()), 'mov')
+            self.vsize = self.vid.get_meta_data()['size']
+            self.vfps = self.vid.get_meta_data()['fps']
+
         self.timer.start()
 
     def present(self):
-        if self.frame_idx == 0:
-            self.Presenter.render(self.grating)
-        self.frame_idx += 1
         if self.timer.elapsed_time() > self.curr_cond['duration']:
             self.isrunning = False
             self.fill()
+            if self.movie: self.vid.close()
+        elif self.movie:
+            grating = pygame.image.frombuffer(self.vid.get_next_data(), self.vsize, "RGB")
+            self.Presenter.render(grating)
+            self.clock.tick_busy_loop(self.vfps)
+        elif self.frame_idx == 0:
+            self.Presenter.render(self.grating)
+        self.frame_idx += 1
 
     def fill(self, color=False):
         if not color:
@@ -98,15 +146,6 @@ class Grating(Stimulus, dj.Manual):
 
     def _gray2rgb(self, im, c=1):
         return np.transpose(np.tile(im, [c, 1, 1]), (1, 2, 0))
-
-    def _calc_destination(self):
-        if self.curr_cond['temporal_freq'] == 0:
-            destination = (0, 0)
-        else:
-            displacement = np.mod(self.frame_idx * self.frame_step, self.curr_cond['lamda'])
-            destination = (-self.curr_cond['lamda'] + self.yt * displacement,
-                           -self.curr_cond['lamda'] + self.xt * displacement)
-        return destination
 
     def _get_filename(self, cond):
         basename = ''.join([c for c in cond['stim_hash'] if c.isalpha()])
@@ -133,152 +172,8 @@ class Grating(Stimulus, dj.Manual):
             im = np.double(im > 0.5)
         return np.uint8(np.floor((im*contrast/100 + (100-contrast)/200)*255))
 
-
-class GratingMovie(Grating):
-    def make_conditions(self, conditions=[]):
-        self.path = os.path.dirname(os.path.abspath(__file__)) + '/movies/'
-        if not os.path.isdir(self.path):  # create path if necessary
-            os.makedirs(self.path)
-        super().make_conditions(conditions)
-        for cond in conditions:
-            filename = self._get_filename(cond)
-            tuple = self.exp.logger.get(schema='stimulus', table='Grating.Movie',
-                                        key={**cond, 'file_name': filename}, fields=['stim_hash'])
-            if not tuple:
-                print('Making movie %s', filename)
-                cond['lamda'] = int(self.px_per_deg/cond['spatial_freq'])
-                theta_frame_step = (cond['temporal_freq'] / self.fps) * np.pi * 2
-                image = self._make_grating(**cond)
-                images = image[:self.monitor['resolution_x'], :self.monitor['resolution_y']]
-                if cond['flatness_correction']:
-                    images, transform = flat2curve(images, self.monitor['monitor_distance'],
-                                                self.monitor['monitor_size'], method='index',
-                                                center_x=self.monitor['monitor_center_x'],
-                                                   center_y=self.monitor['monitor_center_y'])
-                    images = self._gray2rgb(images)
-                else:
-                    transform = lambda x: x
-                for iframe in range(0, int(cond['duration']*self.fps/1000 + 10)):
-                    print('\r' + ('frame %d/%d' % (iframe, int(cond['duration']*self.fps/1000 + 10))), end='')
-                    cond['phase'] += theta_frame_step
-                    image = self._make_grating(**cond)
-                    images = np.dstack((images, self._gray2rgb(transform(image[:self.monitor['resolution_x'],
-                                                                               :self.monitor['resolution_y']]))))
-                print('\r' + 'done!')
-                images = np.transpose(images[:, :, :], [2, 1, 0])
-                self._im2mov(self.path + filename, images)
-                self.logger.log('Grating.Movie', {**cond, 'file_name': filename,
-                                                  'clip': np.fromfile(self.path + filename, dtype=np.int8)},
-                                schema='stimulus', priority=2, block=True, validate=True)
-        return conditions
-
-    def prepare(self, curr_cond):
-        self.curr_cond = curr_cond
-        self.curr_frame = 1
-        self.clock = pygame.time.Clock()
-        self.curr_cond.update(dict(filename=self._get_filename(curr_cond)))
-        clip = self.exp.logger.get(schema='stimulus', table='Grating.Movie', key=self.curr_cond, fields=['clip'])
-        self.vid = imageio.get_reader(io.BytesIO(clip[0].tobytes()), 'mov')
-        self.vsize = self.vid.get_meta_data()['size']
-        self.vfps = self.vid.get_meta_data()['fps']
-        self.isrunning = True
-        self.timer.start()
-
-    def present(self):
-        if self.timer.elapsed_time() < self.curr_cond['duration']:
-            grating = pygame.image.frombuffer(self.vid.get_next_data(),  self.vsize, "RGB")
-            self.Presenter.render(grating)
-            self.curr_frame += 1
-            self.clock.tick_busy_loop(self.vfps)
-        else:
-            self.isrunning = False
-            self.vid.close()
-            #self.fill()
-
     def _im2mov(self, fn, images):
         w = imageio.get_writer(fn, fps=self.fps)
         for frame in images:
             w.append_data(frame)
         w.close()
-
-
-class GratingRP(GratingMovie):
-    """ This class handles the presentation of Gratings with an optimized library for Raspberry pi"""
-
-
-    def setup(self):
-        # setup parameters
-        self.path = os.path.dirname(os.path.abspath(__file__)) + '/movies/'
-        self.size = (self.monitor['resolution_x'], self.monitor['resolution_y'])     # window size
-        self.color = [i*256 for i in self.monitor['background_color']]  # default background color
-        self.phd_size = (50, 50)    # default photodiode signal size in pixels
-
-        # setup pygame
-        if not pygame.get_init():
-            pygame.init()
-        self.screen = pygame.display.set_mode(self.size)
-        self.fill() 
-        pygame.mouse.set_visible(0)
-        pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
-        # setup movies
-        from omxplayer import OMXPlayer
-        self.player = OMXPlayer
-        # store local copy of files
-        if not os.path.isdir(self.path):  # create path if necessary
-            os.makedirs(self.path)
-        for cond in self.conditions:
-            file = self.get_clip_info(cond, 'Grating.Movie', 'file_name')
-            filename = self.path + file[0]
-            if not os.path.isfile(filename):
-                print('Saving %s ...' % filename)
-                clip = self.get_clip_info(cond, 'Grating.Movie', 'clip')
-                clip[0].tofile(filename)
-        # initialize player
-        self.vid = self.player(filename, args=['--aspect-mode', 'stretch', '--no-osd'],
-                    dbus_name='org.mpris.MediaPlayer2.omxplayer1')
-        self.vid.stop()
-
-    def prepare(self, curr_cond, stim_period=''):
-        self.curr_cond = curr_cond
-        self.fill([i*256 for i in self.monitor['start_color']])
-        self._init_player()
-        self.isrunning = True
-        self.timer.start() 
-
-    def present(self):
-        if self.timer.elapsed_time() < self.curr_cond['duration']:
-            try:
-                self.vid.play()
-            except:
-                self._init_player()
-                self.vid.play()
-        else: 
-            self.isrunning = False
-            self.vid.quit()
-
-    def stop(self):
-        try:
-            self.vid.quit()
-        except:
-            self._init_player()
-            self.vid.quit()
-        self.fill([i*256 for i in self.monitor['background_color']])
-        self.log_stop()
-        self.isrunning = False
-
-    def _init_player(self):
-        self.filename = self.path + self.get_clip_info(self.curr_cond, 'Grating.Movie', 'file_name')
-        try:
-            self.vid.load(self.filename[0])
-        except:
-            self.vid = self.player(self.filename[0], args=['--aspect-mode', 'stretch', '--no-osd'],
-                        dbus_name='org.mpris.MediaPlayer2.omxplayer1')
-        self.vid.pause()
-
-    def get_clip_info(self, key, table, *fields):
-        key['file_name'] = self._get_filename(key)
-        return self.exp.logger.get(schema='stimulus', table=table, key=key, fields=fields)
-
-
-
