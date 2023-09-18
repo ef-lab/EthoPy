1828c8a198f23e22c852d2313853841ada2a5557
Emmanouil Froudarakis <olakiril@hotmail.com>
2023-09-18 13:09:01 +0300

    Grating, Movies, Images --> OpenGL same across all interfaces

--- Stimuli/Images.py
+++ Stimuli/Images.py
@@ -1,6 +1,6 @@
 from core.Stimulus import *
 from time import sleep
-import os, imageio
+import os, imageio, cv2
 import numpy as np
 from utils.Presenter import *
 
@@ -38,6 +38,9 @@ class Images(Stimulus, dj.Manual):
         self.curr_cond = curr_cond
         self.clock = pygame.time.Clock()
         curr_img = self._get_image_info(self.curr_cond, 'Image', 'image')
+        curr_img = curr_img[0]
+        if self.upscale != 1:
+            curr_img = cv2.resize(curr_img, dsize=(self.size), interpolation=cv2.INTER_CUBIC)
         img_rgb = curr_img[..., None].repeat(3, -1).astype(np.int32)
         self.curr_img = self.Presenter.make_surface(img_rgb.swapaxes(0, 1))
         image_height, image_width = self._get_image_info(self.curr_cond, 'ImageClass.Info', 'image_height', 'image_width')
