127b0101a6f333bda5094abf8ed293c0ba8c51d4
Emmanouil Froudarakis <olakiril@hotmail.com>
2024-09-02 11:58:19 +0300

    Optogenetic stimulation during object presentation

---
+++ Stimuli/OptoPanda.py
@@ -0,0 +1,32 @@
+from Stimuli.Panda import *
+
+@stimulus.schema
+class Opto(Panda, dj.Manual):
+    definition = """
+    # This class handles the presentation of Opto pulses for optogenetic activity control
+    -> StimCondition
+    ---
+    opt_duration             : int                     # duration (ms)
+    opt_dutycycle            : int                     #  dutycycle
+    """
+
+    cond_tables = ['Opto', 'Panda', 'Panda.Object', 'Panda.Environment', 'Panda.Light', 'Panda.Movie']
+    required_fields = ['obj_id', 'obj_dur', 'opt_duration']
+    default_key = {'background_color': (0, 0, 0),
+                   'ambient_color': (0.1, 0.1, 0.1, 1),
+                   'light_idx': (1, 2),
+                   'light_color': (np.array([0.7, 0.7, 0.7, 1]), np.array([0.2, 0.2, 0.2, 1])),
+                   'light_dir': (np.array([0, -20, 0]), np.array([180, -20, 0])),
+                   'obj_pos_x': 0,
+                   'obj_pos_y': 0,
+                   'obj_mag': .5,
+                   'obj_rot': 0,
+                   'obj_tilt': 0,
+                   'obj_yaw': 0,
+                   'obj_delay': 0,
+                   'obj_occluder': 0,
+                   'opt_dutycycle': 50}
+
+    def start(self):
+        self.exp.interface.opto_stim(self.curr_cond['opt_duration'], self.curr_cond['opt_dutycycle'])
+        super().start()
--- conf/opto_test.py
+++ conf/opto_test.py
@@ -1,30 +1,76 @@
-from Stimuli.Opto import *
-from Experiments.Passive import *
-from Behaviors.HeadFixed import *
+from Stimuli.OptoPanda import *
+from Experiments.MatchToSample import *
+from Behaviors.MultiPort import *
+from scipy import interpolate
+import numpy as np
 
+interp = lambda x: interpolate.splev(np.linspace(0, len(x), 100),
+                                     interpolate.splrep(np.linspace(0, len(x), len(x)), x)) if len(x) > 3 else x
 
 # define session parameters
 session_params = {
-    'setup_conf_idx'     : 1,
-    'trial_selection'    : 'fixed',
+    'start_time'            : '00:00:00',
+    'stop_time'             : '23:59:00',
+    'setup_conf_idx'        : 1,
 }
 
 exp = Experiment()
-exp.setup(logger, HeadFixed, session_params)
+exp.setup(logger, MultiPort, session_params)
 conditions = []
 
-# define stimulus conditions
-dutycycles = (20, 50, 100)
-o_dur = 500
+# two static objects (1 target + 1 distractor) multiple delays & rotation
+cue_obj = [2, 2, 3, 3]
+resp_obj = [(3, 2), (2, 3), (3, 2), (2, 3)]
+rew_prob = [2, 1, 1, 2]
+reps = 1
+panda_obj = Opto()
+panda_obj.fill_colors.set({'background': (0, 0, 0),
+                      'start': (32, 32, 32),
+                      'punish': (0, 0, 0)})
 
-key = {
-    'intertrial_duration': 500,
-}
+block = exp.Block(difficulty=0, next_up=1, next_down=0, trial_selection='staircase', metric='dprime', stair_up=1, stair_down=0.5)
+for irep in range(0, reps):
+    for idx, obj_comb in enumerate(resp_obj):
 
-for ratio in dutycycles:
-    conditions += exp.make_conditions(stim_class=Opto(), conditions={**key,
-                          'dutycycle'      : ratio,
-                          'duration'  : o_dur})
+        # environement & light
+        dir1_f = lambda: np.array([0, -20, 0]) + np.random.randn(3) * 30
+        dir2_f = lambda: np.array([180, -20, 0]) + np.random.randn(3) * 30
+
+        # object parameters
+        rot_f = lambda: interp((np.random.rand(30)-.5) * 250)
+        tilt_f = lambda: interp(np.random.rand(30)*30)
+        yaw_f = lambda: interp(np.random.rand(20)*10)
+
+        conditions += exp.make_conditions(stim_class=panda_obj, stim_periods=['Cue', 'Response'], conditions={**block.dict(),
+            'Cue': {
+                'obj_id'        : cue_obj[idx],
+                'obj_dur'       : 240000,
+                'obj_pos_x'     : 0,
+                'obj_mag'       : .5,
+                'obj_rot'       : (rot_f()),
+                'obj_tilt'      : 0,
+                'obj_yaw'       : 0,
+                'opt_dutycycle'     : 50,
+                'opt_duration'      : [0, 500]},
+            'Response': {
+                'obj_id'        : obj_comb,
+                'obj_dur'       : 240000,
+                'obj_pos_x'     : (-.25, .25),
+                'obj_mag'       : .5,
+                'obj_rot'       : (rot_f(), rot_f()),
+                'obj_tilt'      : 0,
+                'obj_yaw'       : 0,
+                'opt_dutycycle'     : 50,
+                'opt_duration'      : 0},
+            'reward_port'       : rew_prob[idx],
+            'response_port'     : rew_prob[idx],
+            'cue_ready'         : 100,
+            'cue_duration'      : 240000,
+            'delay_duration'    : 100,
+            'response_duration' : 5000,
+            'reward_duration'   : 2000,
+            'punish_duration'   : 500,
+            'reward_amount'     : 6})
 
 
 # run experiments
