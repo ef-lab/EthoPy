33d46d3d4e953fb500f361ae43ff9adcd82ec615
Emmanouil Froudarakis <olakiril@hotmail.com>
2025-02-24 13:48:50 +0200

    Lock recording rec_idx updating to minimize conflicts

--- core/Logger.py
+++ core/Logger.py
@@ -167,7 +167,6 @@ class Logger:
         self.ping_timer = Timer()
         self.logger_timer = Timer()
         self.total_reward = 0
-        self.rec_idx = 0
         self.curr_state = ""
         self.thread_exception = None
         self.update_status = threading.Event()
@@ -1015,18 +1014,15 @@ class Logger:
         The method assumes the existence of a `get` method to retrieve existing recordings
         and a `log` method to log the new recording entry.
         """
-        if not self.rec_idx:  # if rec_idx has not been utilized check database for logs
-            recs = self.get(
-                schema="recording",
-                table="Recording",
-                key=self.trial_key,
-                fields=["rec_idx"],
-            )
-            self.rec_idx = max(recs) + 1
-        else:
-            self.rec_idx += 1
-
-        self.log('Recording', data={**rec_key, 'rec_idx': self.rec_idx}, schema='recording')
+        recs = self.get(
+            schema="recording",
+            table="Recording",
+            key=self.trial_key,
+            fields=["rec_idx"],
+        )
+        rec_idx = 1 if not recs else max(recs) + 1
+        self.log('Recording', data={**rec_key, 'rec_idx': rec_idx}, schema='recording', priority=1, block=True,
+                 validate=True)
 
     def closeDatasets(self):
         """
